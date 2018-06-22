1d811fcac3bb72e8fcc1b9fc54c35a4259eb6257
Dominic <djpkvf@mail.missouri.edu>
2018-06-22 18:55:17 -0500

    Functions Mini-Challenge

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func check(value: Int) -> String? {
+    if value <= 0 {
+        return nil
+    } else {
+        return String(value)
+    }
+}
+
+let numberString1: String? = check(value: 10) // numberString should be "10"
+
+let numberString2: String? = check(value: 0) // numberString should be nil
+
+let numberString3: String? = check(value: -2) // numberString should be nil
